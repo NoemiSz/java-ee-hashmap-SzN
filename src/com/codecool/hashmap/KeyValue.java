b0e3259c16a71ef11baaaee9226403ee6758e472
Noemi Szoke <szokenoemijob@gmail.com>
2019-02-24 15:30:52 +0100

    helper class

---
+++ src/com/codecool/hashmap/KeyValue.java
@@ -0,0 +1,28 @@
+package com.codecool.hashmap;
+
+
+class KeyValue<K, V> {
+    public K key;
+    public V value;
+
+    public KeyValue(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "KeyValue{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+}
